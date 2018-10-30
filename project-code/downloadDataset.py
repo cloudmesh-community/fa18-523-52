3b374470dba28526015097ce8537554abfc9b4b4
Anna Heine <avheine@iu.edu>
2018-10-29 21:35:05 -0400

    download dataset

---
+++ project-code/downloadDataset.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+import urllib.request
+
+webpage = urllib.request.urlretrieve("https://storage.googleapis.com/kaggle-datasets/54339/104884/HAM10000_metadata.csv?GoogleAccessId=web-data@kaggle-161607.iam.gserviceaccount.com&Expires=1541120996&Signature=KjfcWxqffZELbHV9YAamlNMZPAYgBYp3Y4ujrJm0KJa5tTdRgGs1f3329jzUyMXrjSVnEHRKnUKH6bfE0LkMWMRglT2XVK0gcPmBbiXAtuQXLREOWE5OsCXEJEKk798EPe%2Bafno3FmI24vZOEpw6zTsfOcOOaVTEMCn%2FNnNTgx8LUUk37dJxsSjan9zPoZY55fTXLbjRru4NdiaAZykFYJxT8TZ1fLXbVQfelL1l3vnyYJ4UqfDz%2B4vkcln%2FZMyrzwc7yqopeQqfa%2FfU7Qw2HgP2s1qBcnRA2os84SMgA%2BuUAUBH%2F01gVJdBR%2B8h93yiPP%2F6ieNy4O5xTnl89vV4gQ%3D%3D","dataset.csv")
+
