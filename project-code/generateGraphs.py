62ac69edbfcecd6299a8f1234872e68646f8fb1f
Anna Heine <avheine@iu.edu>
2018-11-20 15:13:17 -0500

    new generateGraphs

--- project-code/generateGraphs.py
+++ project-code/generateGraphs.py
@@ -2,8 +2,11 @@
 # coding: utf-8
 
 import pandas as pd
-import matplotlib.pyplot as pp
+import matplotlib.pyplot as plt
 import numpy as np
+from pandas import autocorrelation_plot
+from pandas.plotting import parallel_coordinates
+
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 
@@ -28,3 +31,18 @@ df3 = pd.DataFrame(np.random.randn(1000, 2), columns=['age', 'dx']).cumsum()
 df3['sex'] = pd.Series(list(range(len(db))))
 df3.plot(x='sex', y='age')
 
+#autocorrelation
+df=pd.DataFrame(np.random.randn(1000, 3), columns=['age', 'sex','dx'])
+plt.figure()
+autocorrelation_plot(df)
+
+#scatter matrix
+scatter_matrix(df, alpha=0.2, figsize=(6, 6), diagonal='kde')
+with pd.plotting.plot_params.use('x_compat', True):
+   .....:     df.age.plot(color='r')
+   .....:     df.sex.plot(color='g')
+   .....:     df.dx.plot(color='b')
+df.plot(subplots=True, figsize=(12, 12));
+
+#parallel coordinates
+parallel_coordinates(df, 'dx')
